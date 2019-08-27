e002073e055d21b639c723dc37e1fdcdaf0aea82
Bernd Owsnicki-Klewe <bowsnickiklewe@uwf.edu>
2019-08-27 12:11:13 -0500

    First Demos

---
+++ COP3022Intro/src/SafeRead.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SafeRead {
+	
+	private static int readInt(Scanner inp, String prompt) {
+		System.out.print(prompt + " > ");
+		if (inp.hasNextInt()) {
+			return inp.nextInt();
+		} else {
+			return -1; // terrible for so many reasons
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner inp = new Scanner(System.in);
+		System.out.println("You entered " + readInt(inp,"Enter a number"));
+	}
+}
---
+++ COP3022Intro/src/SafeReadBetter.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SafeReadBetter {
+	
+	private static int readInt(Scanner inp, String prompt) {
+		System.out.print(prompt + " > ");
+		while (!inp.hasNextInt()) {
+			inp.nextLine(); 		// read out whatever is on that line (flush the input)
+			System.out.print(prompt + " > ");
+		}
+		return inp.nextInt();
+	}
+	
+	public static void main(String[] args) {
+		Scanner inp = new Scanner(System.in);
+		System.out.println("You entered " + readInt(inp,"Enter a number"));
+	}
+}
