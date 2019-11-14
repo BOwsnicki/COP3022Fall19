6e3a3d9605ad36698f33d96b5b7e78b21696d213
Bernd Owsnicki-Klewe <bowsnickiklewe@uwf.edu>
2019-11-14 15:47:07 -0600

    More Swing Examples in Swing2

---
+++ Swing2/src/EventObjectDemo.java
@@ -0,0 +1,12 @@
+/**
+   This program creates an instance of the EventObjectWindow
+   class, which causes it to display its window.
+*/
+
+public class EventObjectDemo
+{
+   public static void main(String[] args)
+   {
+      EventObjectWindow eow = new EventObjectWindow();
+   }
+}
